f37ae3bdad8376df6c786ab9509d8db7e4a82b3e
rw422scarlet <35158346+rw422scarlet@users.noreply.github.com>
2018-09-14 00:20:40 -0400

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -41,41 +41,51 @@ else:
 record_row = [] 
 
 # create output csv file 
-title = ['Timestamp', 'Acc X1', 'Acc X2', 'Acc X3', 'Acc Y1', 'Acc Y2', 'Acc Y3', 'Acc Z1', 'Acc Z2', 'Acc Z3']
-file_dir = '/Users/apple/Documents/Documents/GitHub/BARR-SYSTEM/output/record.csv'
+title = ['Timestamp', 'Acc X', 'Acc Y', 'Acc Z', 'Gyr X', 'Gyr Y', 'Gyr Z', 'Mag X', 'Mag Y', 'Mag Z']
+file_dir1 = '/Users/apple/Documents/Documents/GitHub/BARR-SYSTEM/output/sensor1.csv'
+file_dir2 = '/Users/apple/Documents/Documents/GitHub/BARR-SYSTEM/output/sensor2.csv'
 
-with open(file_dir, 'w') as ourput_file:
+with open(file_dir1, 'w') as ourput_file:
+    writer = csv.writer(ourput_file, delimiter = ',', lineterminator='\n')  
+    writer.writerow(title)
+with open(file_dir2, 'w') as ourput_file:
     writer = csv.writer(ourput_file, delimiter = ',', lineterminator='\n')  
     writer.writerow(title)
 
 plt.ion()
 plt.figure
 start = time.time()
-while time.time() - start < 5:
+while time.time() - start < 3:
     tnow = time.time() - start 
-    record_row = I2Creader(ard)
-    record_row.insert(0, tnow)
-    print(record_row)
-    with open(file_dir, 'a') as output_file:
-        csv.writer(output_file).writerow(record_row)     
-        print('Saving to database')       
-
-    data = DataRead(file_dir)  
-
-    plt.clf()
-    # if len(data[0]) <= 15:
+    record1, record2 = I2Creader(ard)
+    record1.insert(0, tnow)
+    record2.insert(0, tnow)
+    # print(len(record1))
+    if len(record1) > 1 or len(record2) > 1:
+    # print(record_row)
+        with open(file_dir1, 'a') as output_file:
+            csv.writer(output_file).writerow(record1)     
+            print('Saving to database')       
+        with open(file_dir2, 'a') as output_file:
+            csv.writer(output_file).writerow(record2)  
+            print('Saving to database')  
     
-    plt.plot(data[0], [i - np.mean(data[1]) for i in data[1]])
-    plt.plot(data[0], [i - np.mean(data[2]) for i in data[4]])
-    plt.plot(data[0], [i - np.mean(data[3]) for i in data[7]])
-    # else: 
-    #     plt.plot(data[0][-15:], data[1][-15:])
-    #     plt.plot(data[0][-15:], data[2][-15:])
-    #     plt.plot(data[0][-15:], data[3][-15:])
-    plt.xlabel('Time (s)')
-    plt.ylabel('Acceleration (g)')
-    # plt.ylim(-2, 2)
-    plt.legend(['X', 'Y', 'Z'])
-    plt.title('IMU Acceleration Plot')
-    plt.pause(0.01)
+    data = DataRead(file_dir1)  
+    print(data)
+    plt.clf()
+    if len(data) > 0:
+    
+        plt.plot(data[0], [i - np.mean(data[1]) for i in data[1]])
+        plt.plot(data[0], [i - np.mean(data[2]) for i in data[2]])
+        plt.plot(data[0], [i - np.mean(data[3]) for i in data[3]])
+        # else: 
+        #     plt.plot(data[0][-15:], data[1][-15:])
+        #     plt.plot(data[0][-15:], data[2][-15:])
+        #     plt.plot(data[0][-15:], data[3][-15:])
+        plt.xlabel('Time (s)')
+        plt.ylabel('Acceleration (g)')
+        # plt.ylim(-2, 2)
+        plt.legend(['X', 'Y', 'Z'])
+        plt.title('IMU Acceleration Plot')
+        plt.pause(0.01)
 ard.close() 
