52404a1864e7fffe7652841e12a9c035c525fbf9
rw422scarlet <35158346+rw422scarlet@users.noreply.github.com>
2018-09-14 00:19:48 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -50,31 +50,63 @@ def IMUreader(Arduino):
         break
     return record_row
 
+# read and parse from arduino 
 def I2Creader(Arduino): 
     line = Arduino.readline().decode('utf-8').replace('\r\n', '')
-    line = line.split(',')
-    print(line)
-    reading = []
-    for element in line: 
-        try:
-            current_element = float(element)
-        except ValueError: 
-            current_element = float('NaN')
-        reading.append(current_element)
-    return reading
+
+    reading1 = reading2 = {"a": None, 'b': None, 'c': None, 'd': None, 'e': None, 'f': None, 'g': None, 'h': None, 'i': None}
+    if 'y' in line: # identify sensor 2 
+        line = line.split('y')
+        if line.count('y') > 1: # if there are multiple sets of sensor 2 readings 
+            line = line[:-1]
+        
+        line2 = line[1].split(',')
+        line1 = line[0].split(',')
+
+        for k in range(len(line1)):
+            current_element = line1[k].split(':')
+            if current_element[0] in reading1: 
+                try:
+                    value = float(current_element[1])
+                except ValueError: 
+                    value = float('NaN')
+                reading1[current_element[0]] = value 
+
+        for k in range(len(line2)):
+            current_element = line2[k].split(':')
+            if current_element[0] in reading1: 
+                try:
+                    value = float(current_element[1])
+                except ValueError: 
+                    value = float('NaN')
+                    print('value converting mistake, exporting')
+                reading2[current_element[0]] = value  
+    
+    reading1 = list(reading1.values())
+    reading2 = list(reading2.values())
+
+    return reading1, reading2
 
 def DataRead(file_dir): 
     with open(file_dir, 'r') as input_file: 
         headers = next(csv.reader(input_file)) 
-
     columns = defaultdict(list)
     print('Pulling from database')
     with open(file_dir, 'r') as input_file: 
         for row in csv.DictReader(input_file): # read a row as {column1: value1, column2: value2,...}
             for (k,v) in row.items(): # go over each column name and value 
                 columns[k].append(v)
-
     raw = []
-    for j in range(len(headers) - 1): 
-        raw.append([float(i) for i in columns[headers[j]]])
+    for j in range(len(headers)): 
+        current = []
+        for i in columns[headers[j]]: 
+            try: 
+                current.append(float(i))
+            except ValueError: 
+                current.append(float('NaN'))
+                print('value converting mistake, importing')
+            except TypeError: 
+                current.append(float('NaN'))
+                print('type converting mistake, importing')
+        raw.append(current)
     return raw
